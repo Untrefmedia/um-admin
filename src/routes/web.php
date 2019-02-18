fb58e5537d9b79fb9e06d3724b0e661be6d2eedc
pablo <pcoronel@untref.edu.ar>
2019-02-18 12:19:16 -0300

    route /admin

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-Route::group(['prefix' => 'admin','middleware'=>['web', 'admin', 'auth:admin']], function ($route) {
-
-//dd($route);
+Route::group(['prefix' => 'admin', 'middleware' => ['web', 'admin', 'auth:admin']], function ($route) {
+    Route::get('/', '\Untrefmedia\UMAdmin\App\Http\Controllers\Admin\DashboardController@index');
     Route::get('dashboard', '\Untrefmedia\UMAdmin\App\Http\Controllers\Admin\DashboardController@index');
-
 });
